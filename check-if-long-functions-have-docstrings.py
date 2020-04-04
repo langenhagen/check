a0d71376365446c62268066bb3862e2b7b05cc5e
Andreas Langenhagen <andreas@langenhagen.cc>
2020-04-04 22:22:36 +0200

    Add check-if-long-functions-have-docstrings.py
    Add a linter that can check whether functions that exceed a given number
    of lines in a given Python file Python have have docstrings.

---
+++ check-if-long-functions-have-docstrings.py
@@ -0,0 +1,62 @@
+#!/usr/env/bin python3
+"""Scan a given Python file and check if long functions have docstrings.
+
+Usage:
+  python check-if-long-functions-have-docstrings.py 50 myfile.py
+
+author: andreasl
+"""
+import ast
+import sys
+
+
+def parse_python_file(filename: str):
+    """Return the ast from a given python file."""
+    with open(filename) as file:
+        root = ast.parse(file.read())
+    return root
+
+
+def get_last_deep_child(ast_node):
+    """Get the last sub-element, deepest sub-node from the given AST node."""
+    if not hasattr(ast_node, "body"):
+        return ast_node
+    return get_last_deep_child(ast_node.body[-1])
+
+
+def find_all_function_nodes(ast_root):
+    """Retrieve all function, also nested, nodes from the given AST node."""
+    if not hasattr(ast_root, "body"):
+        return []
+
+    fun_nodes = []
+    for node in ast_root.body:
+        if isinstance(node, ast.FunctionDef):
+            fun_nodes.append(node)
+        fun_nodes.extend(find_all_function_nodes(node))
+    return fun_nodes
+
+
+def get_node_loc(node):
+    """Get the lines of code that the node and its sub-nodes use."""
+    lineno = node.lineno
+    end_lineno = get_last_deep_child(node).lineno
+    return end_lineno - lineno
+
+
+def run():
+    """Run the linter."""
+    n_lines = int(sys.argv[1])
+    filename = sys.argv[2]
+    root = parse_python_file(filename)
+
+    fun_nodes = find_all_function_nodes(root)
+    for node in fun_nodes:
+        if get_node_loc(node) > n_lines and not ast.get_docstring(node):
+            print(
+                f"{filename}:{node.lineno}:{node.col_offset} A001 function "
+                f"with >{n_lines} LoC lacks docstring")
+
+
+if __name__ == "__main__":
+    run()
