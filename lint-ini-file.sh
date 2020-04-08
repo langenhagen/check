2aa2c7c2a1bf967fbef7ff90e34acb3d694041b1
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-04-08 10:15:09 +0200

    Refactor lint-ini-file.sh

--- lint-ini-file.sh
+++ lint-ini-file.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Identify possible issues in a given ini file.
+#
+# author: andreasl
 
 ini_file="$1"
 
-grep -EHn "[^ ]=" "$ini_file"
-grep -EHn "=[^ ]" "$ini_file"
-grep -EHn ",[[:space:]]" "$ini_file"
+checks_array=(
+    '[^ ]='  # space before assignment
+    '=[^ ]'  # space after assignment
+    ', '  # space after comma
+)
+checks="$(printf '%s|' "${checks_array[@]}")"
+
+grep -EHn "${checks}" "$ini_file"
