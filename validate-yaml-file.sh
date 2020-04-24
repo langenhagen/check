306ca357d2810d7a1c57510416be11103024e6cb
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-04-24 21:22:24 +0200

    Add a comment to validate-yaml-file.sh

--- validate-yaml-file.sh
+++ validate-yaml-file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Validate the given yaml file via python.
 # Requires Python and the Python package pyyaml to be available.
+# Consider using the tool yamllint https://github.com/adrienverge/yamllint.
 #
 # based on:
 # https://stackoverflow.com/questions/3971822/how-do-i-validate-my-yaml-file-from-command-line
