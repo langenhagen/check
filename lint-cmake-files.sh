3bae6e486ca4d9914c0c4959411d83652a67ce21
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-04-06 18:13:30 +0200

    Add lint-cmake-files.sh

---
+++ lint-cmake-files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Lint and optionally format the given CMakeLists.txt files.
+#
+# Call cmake-format and cmake-lint one after another for all given files.
+#
+# Usage:
+#   lint-cmake-files.sh [-f|--format] <FILE> [<FILE>] ...
+#
+# author: andreasl
+
+files=()
+format=
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    -f|--format)
+        format=-i
+        ;;
+    *)
+        files+=("$1")
+        ;;
+    esac
+    shift
+done
+
+for file in "${files[@]}"; do
+    printf '\e[1m=== %s ===\e[m\n' "$file"
+
+    printf '*** cmake-format %s ***\n' "$(cmake-format --version)"
+    cmake-format ${format} "$file"
+
+    printf '*** cmake-lint %s ***\n' "$(cmake-lint --version)"
+    cmake-lint "$file"
+done
